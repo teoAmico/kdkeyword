64630fe6419e3ebaeac878664e301a089305496e
Matteo Amico <teoinbox@outlook.com>
2017-05-27 09:27:08 +0100

    added parser script menu

---
+++ src/Parser.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+use \KDKeywords\Database;
+use \Dotenv\Dotenv;
+use \League\CLImate\CLImate;
+
+
+//load global variable
+$dotenv = new Dotenv(dirname(dirname(__FILE__)));
+$dotenv->load();
+//create terminal instance
+$terminal = new CLImate();
+$terminal->addArt(__DIR__);
+// draw nice logo in terminal
+$terminal->draw('logo');
+// create pdo instance
+$pdo = Database::getInstance($terminal);
+
+//create terminal option configuration
+$terminal->arguments->add([
+
+    'help' => [
+        'prefix' => 'h',
+        'longPrefix' => 'help',
+        'description' => 'Show usage menu',
+        'required' => false,
+        'noValue' => true,
+
+    ],
+    'import' => [
+        'prefix' => 'i',
+        'longPrefix' => 'import',
+        'description' => 'Parse Amazon data feeds and import data into books table',
+        'noValue' => true,
+        'required' => true,
+    ],
+]);
+
+try {
+    $terminal->arguments->parse();
+} catch (\Exception $e) {
+    $terminal->usage();
+    exit;
+}
+
+if ($terminal->arguments->defined('help')) {
+    $terminal->usage();
+    exit;
+}
+
+if ($terminal->arguments->defined('import')) {
+    echo 'TODO parse data'.PHP_EOL;
+    exit();
+}
+
